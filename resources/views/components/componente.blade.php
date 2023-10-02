f7acf8b27fdf41ac3a2a9bb27a213014961c81a3
gregorioooC <125932869+gregorioooC@users.noreply.github.com>
2023-10-02 12:43:41 -0600

    Componentes

---
+++ resources/views/components/componente.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diario Personal</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body style="width:100%; overflow-x:hidden" >
+    {{ $slot }}
+    
+</body>
+</html>
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,64 @@
-@extends('layouts.plantilla')
+<x-componente>
+<div id="carouselExample" class="carousel slide mb-3">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://estaticos.elcolombiano.com/binrepository/1200x800/0c86/1200d627/none/11101/FAYA/shutterstock-189290039_36937209_20210101220826.jpg" class="d-block w-100" height="587">
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.vocaeditorial.com/blog/media/shutterstock_1613766853-e1643212811841-1175x511.jpg" class="d-block w-100">
+    </div>
+    <div class="carousel-item">
+      <img src="https://media.meer.com/attachments/2f10e01cb926f0839296045a6bfa93cf154f2b62/store/fill/1090/613/d8b2fea5b57c51d37192a1be04cc32b660b3e6ed279f2fd37d18df7990e2/Escribir-un-diario.jpg" class="d-block w-100" height="587">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
+<h1 class="display-1 text-danger text-center">Diario Personal</h1>
 
-@section('title') {{'Home'}} @endsection
-
-@section('contenido')
-
- @include('layouts.navbar')
-
-    <h1 class="display-1 text-center mt-5 mb-5 text-dark">Welcome</h1>
-
-
-
-@endsection
+<div class="row row-cols-1 row-cols-md-3 g-4 mt-2 mb-4 px-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://www.lavanguardia.com/files/article_gallery_microformat/uploads/2020/03/09/5fa8fec02294e.jpeg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Recuerdos</h5>
+        <p class="card-text">Relatos sobre experiencias del pasado.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Actualizado hace 6 dias.</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://i.blogs.es/f45165/friendship-1081843_1280/450_1000.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Amigos</h5>
+        <p class="card-text">Aqui cuento los recuerdos que tengo con amigos.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Actualizado hace 3 dias.</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://eldinero.com.do/wp-content/uploads/AdobeStock_280828158-scaled-1.jpeg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Viajes</h5>
+        <p class="card-text">En esta parte hablo de los viajes que he hecho alrededor del mundo.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Actualizado hace 2 meses.</small>
+      </div>
+    </div>
+  </div>
+</div>
+</x-componente>
